33d66f98bc355834ba611df6f22d9249cff27910
刘子豪 <howie.lzh@alibaba-inc.com>
2019-10-19 23:11:14 +0800

    增加棋子图层

--- draw.js
+++ draw.js
@@ -1,9 +1,12 @@
 class Drawer {
   constructor() {
-    this.cvs = document.getElementById('myCanvas')
-    this.ctx = this.cvs.getContext('2d')
+    this.tableCvs = document.getElementById('tableCanvas')
+    this.chessCvs = document.getElementById('chessCanvas')
+    this.tableCtx = this.tableCvs.getContext('2d')
+    this.chessCtx = this.chessCvs.getContext('2d')
     this.sept = 40   //每个格子间隔：40
-    this.ctx.fillStyle='black';
+    this.tableCtx.fillStyle='black';
+    this.chessCtx.fillStyle = 'black';
     this.xNum = 1;
     this.yNum = 1;
     this.chessWidth = 30;   //棋子宽度：30
@@ -15,25 +18,25 @@ class Drawer {
 
   //画棋盘
   initChessBoard() {
-    while(this.curY + this.sept < this.cvs.height) {
+    while(this.curY + this.sept < this.tableCvs.height) {
       this.yNum ++;
       this.curY += this.sept;
     }
-    while(this.curX + this.sept < this.cvs.width) {
+    while(this.curX + this.sept < this.tableCvs.width) {
       this.xNum ++;
       this.curX += this.sept;
     }
     //画横线
     for(let i = 0 ; i < this.yNum; i++) {
-      this.ctx.moveTo(this.startX, this.startY + i * this.sept);
-      this.ctx.lineTo(this.curX, this.startX + i * this.sept);
-      this.ctx.stroke();
+      this.tableCtx.moveTo(this.startX, this.startY + i * this.sept);
+      this.tableCtx.lineTo(this.curX, this.startX + i * this.sept);
+      this.tableCtx.stroke();
     }
     //画竖线
     for(let i = 0; i < this.xNum; i++) {
-      this.ctx.moveTo(this.startX + i * this.sept, this.startY);
-      this.ctx.lineTo(this.startY + i * this.sept, this.curY);
-      this.ctx.stroke();
+      this.tableCtx.moveTo(this.startX + i * this.sept, this.startY);
+      this.tableCtx.lineTo(this.startY + i * this.sept, this.curY);
+      this.tableCtx.stroke();
     }
   }
 
@@ -43,15 +46,15 @@ class Drawer {
     const radius = this.chessWidth / 2;
     const realX = radius + x * this.sept;
     const realY = radius + y * this.sept;
-    this.ctx.beginPath();
-    this.ctx.fillStyle = color;
-    this.ctx.arc(realX, realY, radius, 0, 2 * Math.PI);
-    this.ctx.fill();
+    this.ches.beginPath();
+    this.chessCtx.fillStyle = color;
+    this.chessCtx.arc(realX, realY, radius, 0, 2 * Math.PI);
+    this.chessCtx.fill();
   }
 
   //清空棋子
   clearChess() {
-    this.ctx.clearRect(0, 0, this.cvs.width, this.cvs.height)
+    this.chessCtx.clearRect(0, 0, this.chessCvs.width, this.chessCvs.height)
     this.initChessBoard()
   }
 }
